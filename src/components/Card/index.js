4a080b46a5ee9c8357f1ab1962e01634699979af
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2022-09-13 13:55:03 -0300

    home done

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,19 @@
 import style from "./style.module.css";
 
 export function Card(props) {
-  const { team, year, team_img, team_logo } = props;
+  const { team, year, team_logo } = props;
 
   return (
     <>
-      <div className={style.card}>
-        <div className={style.teamLogo}>
-          <img src={team_logo} alt="Team Logo" />
-        </div>
-        <div className={style.cardNames}>
-            <h1>{team}</h1>
-            <h2>{year}</h2>
+      <div className={style.goldBg}>
+        <div className={style.card}>
+          <div className={style.teamLogo}>
+            <img src={team_logo} alt="Team Logo" />
+          </div>
+          <div className={style.cardNames}>
+              <h1>{team}</h1>
+              <h2>{year}</h2>
+          </div>
         </div>
       </div>
     </>
